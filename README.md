f69f7f220fa0687b099242164522271e3c00055e
shivam kumar jha <shivamjha779@gmail.com>
2021-04-24 10:03:12 +0530

    wrote my first program

---
+++ README.md
@@ -0,0 +1,2 @@
+Hello I am learning C language.
+I am going to add all my important learning practice to this repo.
---
+++ first-step/first.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main() {
+
+    printf("Hello, I am Learning C Language!!\n yeah!!!!\n");
+    return 0;
+}
