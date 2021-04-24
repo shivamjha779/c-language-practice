a42821d2a1fb9a7434606f54a0c5ed3bdc02a4c8
shivam kumar jha <shivamjha779@gmail.com>
2021-04-24 12:05:15 +0530

    wrote scanf and sum

---
+++ first-step/first2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main() {
+
+    int a = 5;
+    float b = 5.789;
+    int c = 8;
+    char e = 's';
+
+    printf("a is a integer, which has value %d \n", a);
+    printf("b is a real no which has a value %f \n", b);
+    printf("the sum of a and b is %f\n", a + b);
+    printf("my name is %c \n", e);
+    return 0;
+}
---
+++ first-step/sum.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main() {
+
+    int a;
+    int b;
+    printf("enter a value a\n");
+    scanf("%d", &a);
+    printf("enter a value b\n");
+    scanf("%d", &b);
+    printf("the sum of the two nos are %d", a + b);
+    return 0;
+
+}
