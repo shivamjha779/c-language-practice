0283011575bc10da4a40f02716b64d66be7a1427
shivam kumar jha <shivamjha779@gmail.com>
2021-07-01 16:49:28 +0530

    string 1

---
+++ imp-c-prog/string.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void print(char a[]){
+    while (*a != '\0')
+    {
+        printf("%c", *a);
+        a++;
+    }
+    
+}
+int main()
+{
+char a[]= "hello";
+print(a);
+return 0;
+}
