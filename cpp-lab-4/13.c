af1a090a3ddd1209d115d97bee860dd148079843
shivam kumar jha <shivamjha779@gmail.com>
2021-07-01 10:58:51 +0530

    lab no4

--- cpp-lab-4/13.c
+++ cpp-lab-4/13.c
@@ -1,15 +1,18 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
-{ // incomplete
-/*Develop a program to print the following output using a single dimensional array
-1 2 3
-4 5 6
-7 8 9 */ 
-int arr[]={1,2,3,4,5,6,7,8,9};
-for(int i=0; i<3; i++){
-    for(int j=i; j<3; j++){
-        printf("%d",arr[i])
+{ 
+    /*Develop a program to print the following output using a single dimensional array
+     1 2 3
+     4 5 6
+     7 8 9 */
+    int arr[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    for (int i = 0; i < 9; i = i + 3)
+    {
+        for (int j = i; j < i + 3; j++)
+        {
+            printf("%d ", arr[j]);
+        }
+        printf("\n");
     }
-}
-return 0;
+    return 0;
 }
